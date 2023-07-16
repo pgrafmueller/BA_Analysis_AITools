c94f66bcefc85705d0d9b84c4c02d84e52c64fc9
Philipp Grafmueller <philipp.grafmueller@gmail.com>
2023-07-16 19:25:56 +0200

    change to jparepository

--- src/main/java/de/thbingen/project/repository/RoleRepository.java
+++ src/main/java/de/thbingen/project/repository/RoleRepository.java
@@ -2,11 +2,11 @@ package de.thbingen.project.repository;
 
 import de.thbingen.project.model.entity.RoleEntity;
 import de.thbingen.project.model.entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface RoleRepository extends CrudRepository<RoleEntity, Long> {
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
     RoleEntity findRoleByName(String roleName);
 
     List<UserEntity> findUsersByRoleContains(RoleEntity role);
