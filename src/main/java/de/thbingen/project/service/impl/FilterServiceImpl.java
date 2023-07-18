d288839bcda4095e8a3159fe1e12ef0d4b4e2a32
Philipp Grafmueller <philipp.grafmueller@gmail.com>
2023-07-18 09:37:48 +0200

    reimplement

--- src/main/java/de/thbingen/project/service/impl/FilterServiceImpl.java
+++ src/main/java/de/thbingen/project/service/impl/FilterServiceImpl.java
@@ -15,67 +15,54 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class FilterServiceImpl implements FilterService {
-        private final UserRepository userRepository;
-        private final RoleRepository roleRepository;
-        private final OrderRepository orderRepository;
 
     @Override
     public List<UserEntity> filterUsersByFirstName(String firstName) {
-        return userRepository.findAll().stream()
-                .filter(userEntity -> userEntity.getFirstName().equals(firstName))
-                .toList();
+        return null;
     }
+
     @Override
     public List<UserEntity> filterUsersByCity(String city) {
-        return userRepository.findAll().stream()
-                .filter(userEntity -> userEntity.getCity().equals(city))
-                .toList();
+        return null;
     }
 
     @Override
     public List<UserEntity> filterUsersByGender(UserEntity.Gender gender) {
-
+        return null;
     }
+
     @Override
     public List<UserEntity> filterUsersByEmail(String email) {
-        return userRepository.findAll().stream()
-                .filter(userEntity -> userEntity.getEmail().equals(email))
-                .toList();
+        return null;
     }
+
     @Override
     public List<UserEntity> filterUsersByPhoneNumber(String phoneNumber) {
-        return userRepository.findAll().stream()
-                .filter(userEntity -> userEntity.getPhoneNumber().equals(phoneNumber))
-                .toList();
+        return null;
     }
+
     @Override
     public List<UserEntity> filterUsersByRole(RoleEntity roleEntity) {
-        return userRepository.findAll().stream()
-                .filter(userEntity -> userEntity.getRoles().contains(roleEntity))
-                .toList();
+        return null;
     }
+
     @Override
     public List<RoleEntity> filterRolesByName(String name) {
-        return roleRepository.findAll().stream()
-                .filter(roleEntity -> roleEntity.getName().equals(name))
-                .toList();
+        return null;
     }
+
     @Override
     public List<RoleEntity> filterRolesByUserCount(int minUserCount) {
-        return roleRepository.findAll().stream()
-                .filter(roleEntity -> roleEntity.getUsers().size() >= minUserCount)
-                .toList();
+        return null;
     }
+
     @Override
     public List<OrderEntity> filterOrdersByAmountRange(double minAmount, double maxAmount) {
-        return orderRepository.findAll().stream()
-                .filter(orderEntity -> orderEntity.getAmount() >= minAmount && orderEntity.getAmount() <= maxAmount)
-                .toList();
+        return null;
     }
+
     @Override
     public List<OrderEntity> filterOrdersByUser(UserEntity userEntity) {
-        return orderRepository.findAll().stream()
-                .filter(orderEntity -> orderEntity.getUser().equals(userEntity))
-                .toList();
+        return null;
     }
 }
